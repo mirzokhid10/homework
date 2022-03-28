ea19b9c6de8be433cbb4003cdfe90ae454eb4cc3
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2022-03-28 15:51:15 +0300

    homework

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let yourName = prompt("Ismingizni kiriting ...")
+let yourAge = prompt("Yoshingizni kiriting ...")
+let exercise1 = prompt("Misolni yeching 6 + 3 = ?")
+let exercise2 = prompt("Misolni yeching 20 - 5 = ?")
+let exercise3 = prompt("Misolni yeching 15 * 2 = ?")
+let exercise4 = prompt("Misolni yeching 8 / 2 = ?")
+let exercise5 = prompt("Misolni yeching 10 % 3 = ?")
+let results = alert("Natijani ko'rish uchun konsolni oching")
+
+console.log(`Mening ismim ${yourName}`);
+console.log(`Mening yoshim ${yourAge}`);
+console.log(`Misol 1:  6 + 3 = 9 Sizning javobingiz ${exercise1}`);
+console.log(`Misol 2: 20 - 5 = 15 Sizning javobingiz ${exercise2}`);
+console.log(`Misol 3: 15 * 2 = 30 Sizning javobingiz ${exercise3}`);
+console.log(`Misol 4: 8 / 2 = 4 Sizning javobingiz ${exercise4}`);
+console.log(`Misol 5: 10 % 3 = 1 Sizning javobingiz ${exercise5}`);
