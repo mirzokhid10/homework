7e8080b42a49dc2d8ef382012f58e2ff2a75b46a
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2022-04-02 18:44:29 +0500

    homework

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-"use strict"
+"use strict";
 
-let yourName = prompt("Ismingizni kiriting ...")
-let yourAge = prompt("Yoshingizni kiriting ...")
-let exercise1 = prompt("Misolni yeching 6 + 3 = ?")
-let exercise2 = prompt("Misolni yeching 20 - 5 = ?")
-let exercise3 = prompt("Misolni yeching 15 * 2 = ?")
-let exercise4 = prompt("Misolni yeching 8 / 2 = ?")
-let exercise5 = prompt("Misolni yeching 10 % 3 = ?")
-let results = alert("Natijani ko'rish uchun konsolni oching")
+let numOne = prompt("1-sonni kiriting ...");
+let numTwo = prompt("2-sonni kiriting ...");
+let numThree = prompt("3-sonni kiriting ...");
 
-console.log(`Mening ismim ${yourName}`);
-console.log(`Mening yoshim ${yourAge}`);
-console.log(`Misol 1:  6 + 3 = 9 Sizning javobingiz ${exercise1}`);
-console.log(`Misol 2: 20 - 5 = 15 Sizning javobingiz ${exercise2}`);
-console.log(`Misol 3: 15 * 2 = 30 Sizning javobingiz ${exercise3}`);
-console.log(`Misol 4: 8 / 2 = 4 Sizning javobingiz ${exercise4}`);
-console.log(`Misol 5: 10 % 3 = 1 Sizning javobingiz ${exercise5}`);
+if (numOne > numTwo && numThree > numOne) {
+  alert(`O'rtacha son qiymati ${numOne}`);
+} else if (numOne > numThree && numTwo > numOne) {
+  alert(`O'rtacha son qiymati ${numOne}`);
+} else if (numTwo > numThree && numOne > numTwo) {
+  alert(`O'rtacha son qiymati ${numTwo}`);
+} else if (numTwo > numOne && numThree > numTwo) {
+  alert(`O'rtacha son qiymati ${numTwo}`);
+} else if (numThree > numOne && numTwo > numThree) {
+  alert(`O'rtacha son qiymati ${numThree}`);
+} else if (numThree > numTwo && numOne > numThree) {
+  alert(`O'rtacha son qiymati ${numThree}`);
+} else {
+  alert(`Yana bir bor urinib ko'ring`);
+}
