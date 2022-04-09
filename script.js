c96fa258273206bf38cf4c44e425d222099b2b74
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2022-04-09 19:14:37 +0500

    objects

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 "use strict";
 
-let numOne = prompt("1-sonni kiriting ...");
-let numTwo = prompt("2-sonni kiriting ...");
-let numThree = prompt("3-sonni kiriting ...");
+let user1 = prompt("Enter your name ...");
+let age1 = prompt("Enter your age ...");
+let user2 = prompt("Enter your name ...");
+let age2 = prompt("Enter your age ...");
 
-if (numOne > numTwo && numThree > numOne) {
-  alert(`O'rtacha son qiymati ${numOne}`);
-} else if (numOne > numThree && numTwo > numOne) {
-  alert(`O'rtacha son qiymati ${numOne}`);
-} else if (numTwo > numThree && numOne > numTwo) {
-  alert(`O'rtacha son qiymati ${numTwo}`);
-} else if (numTwo > numOne && numThree > numTwo) {
-  alert(`O'rtacha son qiymati ${numTwo}`);
-} else if (numThree > numOne && numTwo > numThree) {
-  alert(`O'rtacha son qiymati ${numThree}`);
-} else if (numThree > numTwo && numOne > numThree) {
-  alert(`O'rtacha son qiymati ${numThree}`);
-} else {
-  alert(`Yana bir bor urinib ko'ring`);
+let array = [user1, age1, user2, age2, user3, age3, user4, age4, user5, age5];
+
+for (let i = 1; i < array.length; i++) {
+  alert(i);
+  
 }
+
+let usersInfo = {
+  userOne = {
+
+  }
+};
